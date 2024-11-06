bd7d3ef75ec5d3b172ace915c9720343c2513ca9
Ekaterina <kisaiksa@yandex.ru>
2024-11-06 18:55:20 +0300

    Этап 1

--- stats/service/src/main/java/ru/practicum/ewm/stats/StatsRepository.java
+++ stats/service/src/main/java/ru/practicum/ewm/stats/StatsRepository.java
@@ -12,19 +12,6 @@ import java.util.List;
 @Repository
 public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
 
-//    @Query("SELECT new ru.practicum.ewm.ViewStatsDto(s.app, s.uri, COUNT(s) AS hits) " +
-//            "FROM EndpointHit s " +
-//            "WHERE s.timestamp BETWEEN :start AND :end " +
-//            "AND (:uris IS NULL OR s.uri IN :uris) " +
-//            "GROUP BY s.app, s.uri " +
-//            "HAVING (:unique = false OR COUNT(DISTINCT s.ip) = COUNT(s.ip)) " +
-//            "ORDER BY hits DESC")
-//    List<ViewStatsDto> findStatisticsByParams(
-//            @Param("start") LocalDateTime start,
-//            @Param("end") LocalDateTime end,
-//            @Param("uris") List<String> uris,
-//            @Param("unique") boolean unique);
-
     @Query("SELECT new ru.practicum.ewm.ViewStatsDto(e.app, e.uri, (1L) AS hits) " +
             "FROM EndpointHit e " +
             "WHERE e.timestamp BETWEEN :start AND :end " +
